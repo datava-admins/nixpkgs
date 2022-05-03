d76e7fd294446b85d61aae007ff3510b6f463d2a
Josh Hoffer <jhoffer@sansorgan.es>
2022-05-02 21:28:07 -0600

    sccache: fallback to local build... not sure if some issue with nix sandbox or just issue with concurrent builds

--- nixos/modules/programs/sccache.nix
+++ nixos/modules/programs/sccache.nix
@@ -71,6 +71,7 @@ in {
               export SCCACHE_DIR="${cfg.cacheUrl}"
               export SCCACHE_CACHE_SIZE="${cfg.cacheSize}"
               export SCCACHE_MAX_FRAME_LENGTH=104857600
+              export SCCACHE_IGNORE_SERVER_IO_ERROR=1
               if [ ! -d "$SCCACHE_DIR" ]; then
                 echo "====="
                 echo "Directory '$SCCACHE_DIR' does not exist"
