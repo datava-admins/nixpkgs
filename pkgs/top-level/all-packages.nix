c9c53e4a1079910563e0d60f58874279083403ec
Josh Hoffer <jhoffer@sansorgan.es>
2022-08-22 13:34:13 -0600

    mssql-cli: init at 1.1 (backport from unstable-good)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24194,6 +24194,9 @@ with pkgs;
   mysql_jdbc = callPackage ../servers/sql/mysql/jdbc { };
 
   mssql_jdbc = callPackage ../servers/sql/mssql/jdbc { };
+  
+  mssql-cli = callPackage ../development/tools/mssql-cli { };
+  
   jtds_jdbc = callPackage ../servers/sql/mssql/jdbc/jtds.nix { };
 
   azuredatastudio = callPackage ../applications/misc/azuredatastudio { };
