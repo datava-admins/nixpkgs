e8ebe2349769c9371dc01a1939659209c596928e
Maximilian Bosch <maximilian@mbosch.me>
2023-04-04 09:11:54 +0200

    nixos/tests: default.nix for containers-next

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -129,12 +129,7 @@ in {
   cockroachdb = handleTestOn ["x86_64-linux"] ./cockroachdb.nix {};
   collectd = handleTest ./collectd.nix {};
   consul = handleTest ./consul.nix {};
-  containers-next = handleTest ./containers-next/basic.nix {};
-  containers-next-imperative = handleTest ./containers-next/imperative.nix {};
-  containers-next-macvlan = handleTest ./containers-next/macvlan.nix {};
-  containers-next-migration = handleTest ./containers-next/migration.nix {};
-  containers-next-config-activation = handleTest ./containers-next/config-activation.nix {};
-  containers-next-wireguard = handleTest ./containers-next/wireguard.nix {};
+  containers-next = handleTest ./containers-next {};
   containers-bridge = handleTest ./containers-bridge.nix {};
   containers-custom-pkgs.nix = handleTest ./containers-custom-pkgs.nix {};
   containers-ephemeral = handleTest ./containers-ephemeral.nix {};
