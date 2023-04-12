a5d06ebe4b52494ce89c7dc4801c95554b1d3762
Josh Hoffer <jhoffer@sansorgan.es>
2023-04-12 11:53:26 -0600

    electron-mail: 5.1.6 -> 5.1.7

--- pkgs/applications/networking/mailreaders/electron-mail/default.nix
+++ pkgs/applications/networking/mailreaders/electron-mail/default.nix
@@ -2,12 +2,12 @@
 
 let
   pname = "electron-mail";
-  version = "5.1.6";
+  version = "5.1.7";
   name = "ElectronMail-${version}";
 
   src = fetchurl {
     url = "https://github.com/vladimiry/ElectronMail/releases/download/v${version}/electron-mail-${version}-linux-x86_64.AppImage";
-    sha256 = "sha256-lsXVsx7U43czWFWxAgwTUYTnUXSL4KPFnXLzUklieAo=";
+    sha256 = "sha256-4emQ48fjBWAwZjbePzr0pfs/9qG1iIF/va02MtVR+78=";
   };
 
   appimageContents = appimageTools.extract { inherit name src; };
