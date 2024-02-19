ee472850a789e549792d907388b6350793119575
Nyall Hasty <nhasty@datava.com>
2024-02-19 16:11:23 -0700

    add allow_zero_rows option to sql exporter

--- nixos/modules/services/monitoring/prometheus/exporters/sql.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/sql.nix
@@ -61,6 +61,11 @@ let
         type = listOf str;
         description = "A set of columns that will be used as values of this metric.";
       };
+      allow_zero_rows = mkOption {
+        type = bool;
+        default = false;
+        description = lib.mdDoc "Consider the query failed if it returns zero rows.";
+      };
     };
   };
 
